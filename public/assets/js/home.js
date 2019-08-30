329a32b4e1bb9160fd9e8e81a501530c47303e62
CCastro <chris502castro@gmail.com>
2019-08-30 12:12:59 -0600

    Some styling changes

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -11,38 +11,16 @@ $(".schedule-type").on('click', function () {
     if ( $(".schedule-type").index(this) == 1 ) {
         $("#walk").addClass("active");
         $("#book").removeClass("active");
-        $(".salon-calendar").hide();
+        $(".salon-calendar-container").hide();
         $(".schedule-type-container").css({ marginLeft: '42vw' });
         $("#salon-select-artist").hide();
     } else {
         $("#walk").removeClass("active");
         $("#book").addClass("active");
-        $(".salon-calendar").show();
+        $(".salon-calendar-container").show();
         $(".schedule-type-container").css({ marginLeft: '150px' });
         $("#salon-select-artist").show();
     }
 });
-
-$(".service-card").on('click', function() {
-    $('.service-card.active').removeClass('active');
-    $(`.service-card:eq( ${$(".service-card").index(this)} )`).addClass("active");
-    $(".appt-specifics").html(
-        `<p>Color: </p>
-        <select id='salon-color-select'>
-            <option>This</option>
-            <option>That</option>
-            <option>This</option>
-            <option>That</option>
-        </select>
-        <p>Style</p>
-        <select id='salon-style-select'>
-            <option>This</option>
-            <option>That</option>
-            <option>This</option>
-            <option>That</option>
-        </select>
-        <button type='submit'>Book Now!</button>`
-    )
-})
 
 setActive(0);
